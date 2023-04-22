5f9215e7931772e2355ee0c59bb0fe1f89e2bd0d
ryanmadsenn <97546348+ryanmadsenn@users.noreply.github.com>
2023-04-22 15:00:28 -0600

    Fixed port

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from "express";
+import env from "dotenv";
+env.config();
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Ryan Madsen");
 });
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
