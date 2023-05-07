1f0848e4445b5229c062442ba77937f92f61b775
ryanmadsenn <97546348+ryanmadsenn@users.noreply.github.com>
2023-05-06 18:48:18 -0600

    Personal Assignment 3

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,12 +1,22 @@
+import { Request, Response } from "express";
 import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
 
-const connectDB = async () => {
-  const client = new MongoClient(process.env.DATABASE_URI as string);
-  const response = await client.connect();
-  if (!response) throw new Error("Could not connect to client.");
-  const database = client.db("cse341");
-  if (!database) throw new Error("Could not connect to database.");
-  return database;
+const connectDB = async (req: Request, res: Response) => {
+  try {
+    const client = new MongoClient(process.env.DATABASE_URI as string);
+    const response = await client.connect();
+    if (!response) throw new Error("Could not connect to client.");
+    const database = client.db("cse341");
+    if (!database) throw new Error("Could not connect to database.");
+    return database;
+  } catch (error: Error | unknown) {
+    /* eslint-disable-next-line */
+    console.log(error);
+    res.status(500).send("Could not connect to database.");
+    return;
+  }
 };
 
 export { connectDB };
--- src/routes/contacts.ts
+++ src/routes/contacts.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
-import { getContacts, getContact } from "../controllers/contactsController";
+import {
+  getContacts,
+  getContact,
+  createContact,
+  updateContact,
+  deleteContact,
+} from "../controllers/contactsController";
 const contactsRoute = Router();
 
 contactsRoute.get("/", getContacts);
 contactsRoute.get("/:id", getContact);
+contactsRoute.post("/", createContact);
+contactsRoute.put("/:id", updateContact);
+contactsRoute.delete("/:id", deleteContact);
 
 export default contactsRoute;
