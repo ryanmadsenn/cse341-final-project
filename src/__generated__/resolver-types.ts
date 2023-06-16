4dcfe007d37f19c6c7622a4cb8c92ecbc47fca8e
ryanmadsenn <97546348+ryanmadsenn@users.noreply.github.com>
2023-06-16 08:32:32 -0600

    GraphQL Schema

---
+++ src/__generated__/resolver-types.ts
@@ -0,0 +1,493 @@
+import { GraphQLResolveInfo } from 'graphql';
+import { Context } from '../server';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type RequireFields<T, K extends keyof T> = Omit<T, K> & { [P in K]-?: NonNullable<T[P]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type Event = {
+  __typename?: 'Event';
+  datetime: Scalars['String'];
+  description: Scalars['String'];
+  id: Scalars['ID'];
+  title: Scalars['String'];
+  users?: Maybe<Array<Maybe<User>>>;
+  vendors?: Maybe<Array<Maybe<Vendor>>>;
+  venue: Scalars['ID'];
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  createEvent?: Maybe<Event>;
+  createUser?: Maybe<User>;
+  createVendor?: Maybe<Vendor>;
+  createVenue?: Maybe<Venue>;
+  deleteEvent?: Maybe<Event>;
+  deleteUser?: Maybe<User>;
+  deleteVendor?: Maybe<Vendor>;
+  deleteVenue?: Maybe<Venue>;
+  updateEvent?: Maybe<Event>;
+  updateUser?: Maybe<User>;
+  updateVendor?: Maybe<Vendor>;
+  updateVenue?: Maybe<Venue>;
+};
+
+
+export type MutationCreateEventArgs = {
+  datetime: Scalars['String'];
+  description: Scalars['String'];
+  title: Scalars['String'];
+  venue: Scalars['ID'];
+};
+
+
+export type MutationCreateUserArgs = {
+  email: Scalars['String'];
+  fname: Scalars['String'];
+  lname: Scalars['String'];
+  password: Scalars['String'];
+  phone: Scalars['String'];
+  role: Role;
+};
+
+
+export type MutationCreateVendorArgs = {
+  address: Scalars['String'];
+  city: Scalars['String'];
+  country: Scalars['String'];
+  description: Scalars['String'];
+  email: Scalars['String'];
+  name: Scalars['String'];
+  phone: Scalars['String'];
+  state: Scalars['String'];
+  website: Scalars['String'];
+  zip: Scalars['String'];
+};
+
+
+export type MutationCreateVenueArgs = {
+  address: Scalars['String'];
+  city: Scalars['String'];
+  country: Scalars['String'];
+  description: Scalars['String'];
+  email: Scalars['String'];
+  name: Scalars['String'];
+  phone: Scalars['String'];
+  state: Scalars['String'];
+  website: Scalars['String'];
+  zip: Scalars['String'];
+};
+
+
+export type MutationDeleteEventArgs = {
+  id: Scalars['ID'];
+};
+
+
+export type MutationDeleteUserArgs = {
+  id: Scalars['ID'];
+};
+
+
+export type MutationDeleteVendorArgs = {
+  id: Scalars['ID'];
+};
+
+
+export type MutationDeleteVenueArgs = {
+  id: Scalars['ID'];
+};
+
+
+export type MutationUpdateEventArgs = {
+  datetime: Scalars['String'];
+  description: Scalars['String'];
+  id: Scalars['ID'];
+  title: Scalars['String'];
+  venue: Scalars['ID'];
+};
+
+
+export type MutationUpdateUserArgs = {
+  email: Scalars['String'];
+  fname: Scalars['String'];
+  id: Scalars['ID'];
+  lname: Scalars['String'];
+  password: Scalars['String'];
+  phone: Scalars['String'];
+  role: Role;
+};
+
+
+export type MutationUpdateVendorArgs = {
+  address: Scalars['String'];
+  city: Scalars['String'];
+  country: Scalars['String'];
+  description: Scalars['String'];
+  email: Scalars['String'];
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  phone: Scalars['String'];
+  state: Scalars['String'];
+  website: Scalars['String'];
+  zip: Scalars['String'];
+};
+
+
+export type MutationUpdateVenueArgs = {
+  address: Scalars['String'];
+  city: Scalars['String'];
+  country: Scalars['String'];
+  description: Scalars['String'];
+  email: Scalars['String'];
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  phone: Scalars['String'];
+  state: Scalars['String'];
+  website: Scalars['String'];
+  zip: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  event?: Maybe<Event>;
+  events?: Maybe<Array<Maybe<Event>>>;
+  user?: Maybe<User>;
+  users?: Maybe<Array<Maybe<User>>>;
+  vendor?: Maybe<Vendor>;
+  vendors?: Maybe<Array<Maybe<Vendor>>>;
+  venue?: Maybe<Venue>;
+  venues?: Maybe<Array<Maybe<Venue>>>;
+};
+
+
+export type QueryEventArgs = {
+  id: Scalars['ID'];
+};
+
+
+export type QueryUserArgs = {
+  id: Scalars['ID'];
+};
+
+
+export type QueryVendorArgs = {
+  id: Scalars['ID'];
+};
+
+
+export type QueryVenueArgs = {
+  id: Scalars['ID'];
+};
+
+export enum Role {
+  Admin = 'ADMIN',
+  User = 'USER'
+}
+
+export type User = {
+  __typename?: 'User';
+  email: Scalars['String'];
+  events?: Maybe<Array<Maybe<Event>>>;
+  fname: Scalars['String'];
+  id: Scalars['ID'];
+  lname: Scalars['String'];
+  password: Scalars['String'];
+  phone: Scalars['String'];
+  role: Role;
+};
+
+export type Vendor = {
+  __typename?: 'Vendor';
+  address: Scalars['String'];
+  city: Scalars['String'];
+  country: Scalars['String'];
+  description: Scalars['String'];
+  email: Scalars['String'];
+  events?: Maybe<Array<Maybe<Event>>>;
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  phone: Scalars['String'];
+  state: Scalars['String'];
+  website: Scalars['String'];
+  zip: Scalars['String'];
+};
+
+export type Venue = {
+  __typename?: 'Venue';
+  address: Scalars['String'];
+  city: Scalars['String'];
+  country: Scalars['String'];
+  description: Scalars['String'];
+  email: Scalars['String'];
+  events?: Maybe<Array<Maybe<Event>>>;
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  phone: Scalars['String'];
+  state: Scalars['String'];
+  website: Scalars['String'];
+  zip: Scalars['String'];
+};
+
+export type AdditionalEntityFields = {
+  path?: InputMaybe<Scalars['String']>;
+  type?: InputMaybe<Scalars['String']>;
+};
+
+
+
+export type ResolverTypeWrapper<T> = Promise<T> | T;
+
+
+export type ResolverWithResolve<TResult, TParent, TContext, TArgs> = {
+  resolve: ResolverFn<TResult, TParent, TContext, TArgs>;
+};
+export type Resolver<TResult, TParent = {}, TContext = {}, TArgs = {}> = ResolverFn<TResult, TParent, TContext, TArgs> | ResolverWithResolve<TResult, TParent, TContext, TArgs>;
+
+export type ResolverFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Promise<TResult> | TResult;
+
+export type SubscriptionSubscribeFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => AsyncIterable<TResult> | Promise<AsyncIterable<TResult>>;
+
+export type SubscriptionResolveFn<TResult, TParent, TContext, TArgs> = (
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+export interface SubscriptionSubscriberObject<TResult, TKey extends string, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<{ [key in TKey]: TResult }, TParent, TContext, TArgs>;
+  resolve?: SubscriptionResolveFn<TResult, { [key in TKey]: TResult }, TContext, TArgs>;
+}
+
+export interface SubscriptionResolverObject<TResult, TParent, TContext, TArgs> {
+  subscribe: SubscriptionSubscribeFn<any, TParent, TContext, TArgs>;
+  resolve: SubscriptionResolveFn<TResult, any, TContext, TArgs>;
+}
+
+export type SubscriptionObject<TResult, TKey extends string, TParent, TContext, TArgs> =
+  | SubscriptionSubscriberObject<TResult, TKey, TParent, TContext, TArgs>
+  | SubscriptionResolverObject<TResult, TParent, TContext, TArgs>;
+
+export type SubscriptionResolver<TResult, TKey extends string, TParent = {}, TContext = {}, TArgs = {}> =
+  | ((...args: any[]) => SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>)
+  | SubscriptionObject<TResult, TKey, TParent, TContext, TArgs>;
+
+export type TypeResolveFn<TTypes, TParent = {}, TContext = {}> = (
+  parent: TParent,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => Maybe<TTypes> | Promise<Maybe<TTypes>>;
+
+export type IsTypeOfResolverFn<T = {}, TContext = {}> = (obj: T, context: TContext, info: GraphQLResolveInfo) => boolean | Promise<boolean>;
+
+export type NextResolverFn<T> = () => Promise<T>;
+
+export type DirectiveResolverFn<TResult = {}, TParent = {}, TContext = {}, TArgs = {}> = (
+  next: NextResolverFn<TResult>,
+  parent: TParent,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo
+) => TResult | Promise<TResult>;
+
+
+
+/** Mapping between all available schema types and the resolvers types */
+export type ResolversTypes = {
+  Event: ResolverTypeWrapper<Event>;
+  String: ResolverTypeWrapper<Scalars['String']>;
+  ID: ResolverTypeWrapper<Scalars['ID']>;
+  Mutation: ResolverTypeWrapper<{}>;
+  Query: ResolverTypeWrapper<{}>;
+  Role: Role;
+  User: ResolverTypeWrapper<User>;
+  Vendor: ResolverTypeWrapper<Vendor>;
+  Venue: ResolverTypeWrapper<Venue>;
+  AdditionalEntityFields: AdditionalEntityFields;
+  Boolean: ResolverTypeWrapper<Scalars['Boolean']>;
+};
+
+/** Mapping between all available schema types and the resolvers parents */
+export type ResolversParentTypes = {
+  Event: Event;
+  String: Scalars['String'];
+  ID: Scalars['ID'];
+  Mutation: {};
+  Query: {};
+  User: User;
+  Vendor: Vendor;
+  Venue: Venue;
+  AdditionalEntityFields: AdditionalEntityFields;
+  Boolean: Scalars['Boolean'];
+};
+
+export type UnionDirectiveArgs = {
+  discriminatorField?: Maybe<Scalars['String']>;
+  additionalFields?: Maybe<Array<Maybe<AdditionalEntityFields>>>;
+};
+
+export type UnionDirectiveResolver<Result, Parent, ContextType = Context, Args = UnionDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type AbstractEntityDirectiveArgs = {
+  discriminatorField: Scalars['String'];
+  additionalFields?: Maybe<Array<Maybe<AdditionalEntityFields>>>;
+};
+
+export type AbstractEntityDirectiveResolver<Result, Parent, ContextType = Context, Args = AbstractEntityDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type EntityDirectiveArgs = {
+  embedded?: Maybe<Scalars['Boolean']>;
+  additionalFields?: Maybe<Array<Maybe<AdditionalEntityFields>>>;
+};
+
+export type EntityDirectiveResolver<Result, Parent, ContextType = Context, Args = EntityDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type ColumnDirectiveArgs = {
+  overrideType?: Maybe<Scalars['String']>;
+};
+
+export type ColumnDirectiveResolver<Result, Parent, ContextType = Context, Args = ColumnDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type IdDirectiveArgs = { };
+
+export type IdDirectiveResolver<Result, Parent, ContextType = Context, Args = IdDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type LinkDirectiveArgs = {
+  overrideType?: Maybe<Scalars['String']>;
+};
+
+export type LinkDirectiveResolver<Result, Parent, ContextType = Context, Args = LinkDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type EmbeddedDirectiveArgs = { };
+
+export type EmbeddedDirectiveResolver<Result, Parent, ContextType = Context, Args = EmbeddedDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type MapDirectiveArgs = {
+  path: Scalars['String'];
+};
+
+export type MapDirectiveResolver<Result, Parent, ContextType = Context, Args = MapDirectiveArgs> = DirectiveResolverFn<Result, Parent, ContextType, Args>;
+
+export type EventResolvers<ContextType = Context, ParentType extends ResolversParentTypes['Event'] = ResolversParentTypes['Event']> = {
+  datetime?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  description?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  title?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  users?: Resolver<Maybe<Array<Maybe<ResolversTypes['User']>>>, ParentType, ContextType>;
+  vendors?: Resolver<Maybe<Array<Maybe<ResolversTypes['Vendor']>>>, ParentType, ContextType>;
+  venue?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type MutationResolvers<ContextType = Context, ParentType extends ResolversParentTypes['Mutation'] = ResolversParentTypes['Mutation']> = {
+  createEvent?: Resolver<Maybe<ResolversTypes['Event']>, ParentType, ContextType, RequireFields<MutationCreateEventArgs, 'datetime' | 'description' | 'title' | 'venue'>>;
+  createUser?: Resolver<Maybe<ResolversTypes['User']>, ParentType, ContextType, RequireFields<MutationCreateUserArgs, 'email' | 'fname' | 'lname' | 'password' | 'phone' | 'role'>>;
+  createVendor?: Resolver<Maybe<ResolversTypes['Vendor']>, ParentType, ContextType, RequireFields<MutationCreateVendorArgs, 'address' | 'city' | 'country' | 'description' | 'email' | 'name' | 'phone' | 'state' | 'website' | 'zip'>>;
+  createVenue?: Resolver<Maybe<ResolversTypes['Venue']>, ParentType, ContextType, RequireFields<MutationCreateVenueArgs, 'address' | 'city' | 'country' | 'description' | 'email' | 'name' | 'phone' | 'state' | 'website' | 'zip'>>;
+  deleteEvent?: Resolver<Maybe<ResolversTypes['Event']>, ParentType, ContextType, RequireFields<MutationDeleteEventArgs, 'id'>>;
+  deleteUser?: Resolver<Maybe<ResolversTypes['User']>, ParentType, ContextType, RequireFields<MutationDeleteUserArgs, 'id'>>;
+  deleteVendor?: Resolver<Maybe<ResolversTypes['Vendor']>, ParentType, ContextType, RequireFields<MutationDeleteVendorArgs, 'id'>>;
+  deleteVenue?: Resolver<Maybe<ResolversTypes['Venue']>, ParentType, ContextType, RequireFields<MutationDeleteVenueArgs, 'id'>>;
+  updateEvent?: Resolver<Maybe<ResolversTypes['Event']>, ParentType, ContextType, RequireFields<MutationUpdateEventArgs, 'datetime' | 'description' | 'id' | 'title' | 'venue'>>;
+  updateUser?: Resolver<Maybe<ResolversTypes['User']>, ParentType, ContextType, RequireFields<MutationUpdateUserArgs, 'email' | 'fname' | 'id' | 'lname' | 'password' | 'phone' | 'role'>>;
+  updateVendor?: Resolver<Maybe<ResolversTypes['Vendor']>, ParentType, ContextType, RequireFields<MutationUpdateVendorArgs, 'address' | 'city' | 'country' | 'description' | 'email' | 'id' | 'name' | 'phone' | 'state' | 'website' | 'zip'>>;
+  updateVenue?: Resolver<Maybe<ResolversTypes['Venue']>, ParentType, ContextType, RequireFields<MutationUpdateVenueArgs, 'address' | 'city' | 'country' | 'description' | 'email' | 'id' | 'name' | 'phone' | 'state' | 'website' | 'zip'>>;
+};
+
+export type QueryResolvers<ContextType = Context, ParentType extends ResolversParentTypes['Query'] = ResolversParentTypes['Query']> = {
+  event?: Resolver<Maybe<ResolversTypes['Event']>, ParentType, ContextType, RequireFields<QueryEventArgs, 'id'>>;
+  events?: Resolver<Maybe<Array<Maybe<ResolversTypes['Event']>>>, ParentType, ContextType>;
+  user?: Resolver<Maybe<ResolversTypes['User']>, ParentType, ContextType, RequireFields<QueryUserArgs, 'id'>>;
+  users?: Resolver<Maybe<Array<Maybe<ResolversTypes['User']>>>, ParentType, ContextType>;
+  vendor?: Resolver<Maybe<ResolversTypes['Vendor']>, ParentType, ContextType, RequireFields<QueryVendorArgs, 'id'>>;
+  vendors?: Resolver<Maybe<Array<Maybe<ResolversTypes['Vendor']>>>, ParentType, ContextType>;
+  venue?: Resolver<Maybe<ResolversTypes['Venue']>, ParentType, ContextType, RequireFields<QueryVenueArgs, 'id'>>;
+  venues?: Resolver<Maybe<Array<Maybe<ResolversTypes['Venue']>>>, ParentType, ContextType>;
+};
+
+export type UserResolvers<ContextType = Context, ParentType extends ResolversParentTypes['User'] = ResolversParentTypes['User']> = {
+  email?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  events?: Resolver<Maybe<Array<Maybe<ResolversTypes['Event']>>>, ParentType, ContextType>;
+  fname?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  lname?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  password?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  phone?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  role?: Resolver<ResolversTypes['Role'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type VendorResolvers<ContextType = Context, ParentType extends ResolversParentTypes['Vendor'] = ResolversParentTypes['Vendor']> = {
+  address?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  city?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  country?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  description?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  email?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  events?: Resolver<Maybe<Array<Maybe<ResolversTypes['Event']>>>, ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  phone?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  state?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  website?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  zip?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type VenueResolvers<ContextType = Context, ParentType extends ResolversParentTypes['Venue'] = ResolversParentTypes['Venue']> = {
+  address?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  city?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  country?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  description?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  email?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  events?: Resolver<Maybe<Array<Maybe<ResolversTypes['Event']>>>, ParentType, ContextType>;
+  id?: Resolver<ResolversTypes['ID'], ParentType, ContextType>;
+  name?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  phone?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  state?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  website?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  zip?: Resolver<ResolversTypes['String'], ParentType, ContextType>;
+  __isTypeOf?: IsTypeOfResolverFn<ParentType, ContextType>;
+};
+
+export type Resolvers<ContextType = Context> = {
+  Event?: EventResolvers<ContextType>;
+  Mutation?: MutationResolvers<ContextType>;
+  Query?: QueryResolvers<ContextType>;
+  User?: UserResolvers<ContextType>;
+  Vendor?: VendorResolvers<ContextType>;
+  Venue?: VenueResolvers<ContextType>;
+};
+
+export type DirectiveResolvers<ContextType = Context> = {
+  union?: UnionDirectiveResolver<any, any, ContextType>;
+  abstractEntity?: AbstractEntityDirectiveResolver<any, any, ContextType>;
+  entity?: EntityDirectiveResolver<any, any, ContextType>;
+  column?: ColumnDirectiveResolver<any, any, ContextType>;
+  id?: IdDirectiveResolver<any, any, ContextType>;
+  link?: LinkDirectiveResolver<any, any, ContextType>;
+  embedded?: EmbeddedDirectiveResolver<any, any, ContextType>;
+  map?: MapDirectiveResolver<any, any, ContextType>;
+};
+
+import { ObjectId } from 'mongodb';
--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,64 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express, { json } from "express";
+import http from "http";
+import cors from "cors";
+import { auth } from "express-oauth2-jwt-bearer";
 import { readFileSync } from "fs";
 import { resolvers } from "./resolvers.js";
 import { Db } from "mongodb";
 import { connectDB } from "./connect.js";
+
 const typeDefs = readFileSync("./schema.graphql", "utf-8");
 const db = await connectDB();
 
+// const verifyJWT = auth({
+//   audience: process.env.AUTH0_AUDIENCE as string,
+//   issuerBaseURL: process.env.AUTH0_DOMAIN as string,
+// });
 export interface Context {
   dataSources: {
     db: Db;
   };
+  isAuthenticated: boolean;
 }
 
+const app = express();
+const httpServer = http.createServer(app);
 const server = new ApolloServer<Context>({
   typeDefs,
   resolvers,
   introspection: true,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
+await server.start();
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: Number(process.env.PORT) || 4000 },
-  context: async () => {
-    return { dataSources: { db } };
-  },
-});
+app.use(
+  "/graphql",
+  cors<cors.CorsRequest>(),
+  json(),
+  expressMiddleware(server, {
+    context: async ({ req, res }) => {
+      // await verifyJWT(req, res, (err) => {
+      //   if (err) {
+      //     console.error(err);
+      //   }
+      // });
+
+      return {
+        dataSources: {
+          db,
+        },
+        isAuthenticated: !!req.auth,
+      };
+    },
+  })
+);
+
+await new Promise<void>((resolve) =>
+  httpServer.listen({ port: process.env.PORT }, resolve)
+);
+
 // eslint-disable-next-line no-console
-console.log(`🚀 Server ready at ${url}`);
+console.log(`🚀 Server ready at http://localhost:${process.env.PORT}/graphql`);
