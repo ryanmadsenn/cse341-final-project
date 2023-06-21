3d86ad0c2dab66c6ada478f63801830c38055d77
ryanmadsenn <97546348+ryanmadsenn@users.noreply.github.com>
2023-06-20 20:45:01 -0600

    Resolver demo

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,6 +1,20 @@
 import { Resolvers } from "./__generated__/resolver-types";
 
 export const resolvers: Resolvers = {
-  Query: {},
+  Query: {
+    events: async (_, __, { dataSources }) => {
+      return [
+        {
+          id: "1",
+          description: "test",
+          title: "test",
+          datetime: "test",
+          venue: "test",
+          users: [],
+          vendors: [],
+        },
+      ];
+    },
+  },
   Mutation: {},
 };
